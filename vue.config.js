c9228eda7465bddb828377d9cb93c5667e8cb6b5
hyeonguj <jhg3182@naver.com>
2021-09-05 01:25:48 +0900

    vue dev proxy

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 module.exports = {
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8089', // this configuration needs to correspond to the Spring Boot backends' application.properties server.port
+        ws: true,
+        changeOrigin: true
+      }
+    }
+  },
   transpileDependencies: [
     'vuetify'
   ]
